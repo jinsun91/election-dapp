dbe3f0cbe3577d55bd25623f1418c533143c7e76
jinsun91 <jinsunpark917@gmail.com>
2018-10-29 10:13:47 +0000

    Created front-end client and added tests

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,30 @@
 pragma solidity ^0.4.2;
 
 contract Election {
-    // Store candidate
-    // Read candidate
-    string public candidate;
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Store Candidates
+    mapping(uint => Candidate) public candidates;
+
+    // Fetch Candidates
+    // Store Candidates Count
+    uint public candidatesCount;
+
     // Constructor - run whenever we deploy our contract to the blockchain so we must make it public
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    // Make this function private because we don't want anyone to be able to call this function from outside the smart contract
+    // We want our smart contract to be in control of this
+    function addCandidate(string _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
