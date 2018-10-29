0d96f0276d7854c243c42430b38df3d7864e13c2
jinsun91 <jinsunpark917@gmail.com>
2018-10-29 11:34:06 +0000

    Implemented cast vote functionality

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -11,6 +11,9 @@ contract Election {
     // Store Candidates
     mapping(uint => Candidate) public candidates;
 
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+
     // Fetch Candidates
     // Store Candidates Count
     uint public candidatesCount;
@@ -26,5 +29,20 @@ contract Election {
     function addCandidate(string _name) private {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        // msg.sender is metadata sent through the vote function that refers to the account sending this function
+        voters[msg.sender] = true;
+
+        // update candidate vote count
+        candidates[_candidateId].voteCount++;
     }
 }
